3da34e319ec16bd78176fada178b0c0ea01ffb7e
Jose Manuel <castellanogonzalezjosemanuel@gmail.com>
2020-09-27 10:57:26 +0200

    snake moves itself

--- scripts/index.js
+++ scripts/index.js
@@ -12,12 +12,14 @@ for (i = 0; i < 240; i++) {
   cells.push(cell);
 
 }
-let headPosition = 220;
+
 //create the snake
 
 const addsnake = (index) => cells[index].classList.add('snake');
 
 //addsnake(2);
+
+headPosition = 220;
 const x = headPosition % 10;
 const y = Math.floor(headPosition / 10);
 
@@ -37,32 +39,56 @@ const handleKeyPress = (event) => {
   switch (key) {
     case 'ArrowUp':
       if (y > 0) {
-        headPosition = headPosition - width;
+        moveUp();
       }
       break;
     case 'ArrowRight':
       if (x < width - 1) {
-        headPosition++;
+        moveRight();
       }
       break;
     case 'ArrowDown':
       if (y < width - 1) {
-        headPosition = headPosition + width;
+        moveDowun();
       }
       break;
     case 'ArrowLeft':
       if (x > 0) {
-        headPosition--;
+        moveLeft();
       }
 
       break;
     default:
-      console.log('Eso no es una flecha flaco');
+
       break;
   }
   addsnake(headPosition);
 };
 
+function moveUp() {
+  window.setInterval(function () {
+    headPosition = headPosition - width;
+    addsnake(headPosition);
+  }, 1000);
+}
+function moveDown() {
+  window.setInterval(function () {
+    headPosition = headPosition + width;
+    addsnake(headPosition);
+  }, 1000);
+}
+function moveLeft() {
+  window.setInterval(function () {
+    headPosition--;
+    addsnake(headPosition);
+  }, 1000);
+}
+function moveRight() {
+  window.setInterval(function () {
+    headPosition++;
+    addsnake(headPosition);
+  }, 1000);
+}
 //addSnake(headPosition);
 
-window.addEventListener('keyup', handleKeyPress);
+window.addEventListener('keydown', handleKeyPress);
